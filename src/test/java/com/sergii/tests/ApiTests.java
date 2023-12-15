98be5b0a4a6652e70ccf94ae26b19db9b1044710
chygraiser <s.chygrai@gamil.com>
2023-12-15 17:48:41 +0100

    Home Work 19 - Api Test Get done

--- src/test/java/com/sergii/tests/ApiTests.java
+++ src/test/java/com/sergii/tests/ApiTests.java
@@ -38,11 +38,36 @@ public class ApiTests extends BaseTest {
 
 
        Root expectedData = Root.builder()
-               .data(Data.builder().build())
-               .support(Support.builder().build())
+               .data(Data.builder()
+                       .id(2).name("fuchsia rose")
+                       .year(2001)
+                       .color("#C74375")
+                       .pantone_value("17-2031")
+                       .build())
+               .support(Support.builder()
+                       .url("https://reqres.in/#support-heading")
+                       .text("To keep ReqRes free, contributions towards server costs are appreciated!")
+                       .build())
                .build();
 
+
        Assert.assertEquals(rootObject, expectedData, "Data are not equals");
+    }
+
+
+    @Test
+    public void postCreate() {
+
+        given()
+                .body()
+                .when()
+                .post("/api/users")
+                .then()
+                .statusCode(201)
+                .log()
+                .all();
+
+
     }
 
 }
